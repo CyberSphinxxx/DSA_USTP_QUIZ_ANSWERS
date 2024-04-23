14189755086301bcd022748f472f86f4b05d5fa0
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:58:17 +0800

    Creating a Tuple with Nested Lists

---
+++ NestedListTuples.py
@@ -0,0 +1,7 @@
+# Write a variable named: my_tuple 
+# Inside this tuple, put Nested Lists as values. 
+# Write the code below.
+
+my_tuple = (["gwapo", "pogi", "handsome"], ["batak", "magaling", "expert"], ["hello", "world", "hi"])
+
+print(my_tuple)
