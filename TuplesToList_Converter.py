ac3c6dfb066af7ff3a1caaf01bcf817f4a0c2178
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:29:08 +0800

    Converting Tuples to Lists in Python

---
+++ TuplesToList_Converter.py
@@ -0,0 +1,14 @@
+#From the given statements: 
+#my_grade = [1.2, 1.5, 2.0, 1.0, 1.8]
+#my_subjects = ("English", "Math", "Science", "ROTC", "CWTS")
+
+#How to convert the tuples to list from the given samples of values? 
+#Write the code in the space below. 
+
+my_grade = [1.2, 1.5, 2.0, 1.0, 1.8]
+my_subjects = ("English", "Math", "Science", "ROTC", "CWTS")
+
+my_subjects_list = list(my_subjects)
+
+print("\"my_grade\" is already a Tuple:", my_grade)
+print("Tuple \"my_subjects\" converted to list:", my_subjects_list)
