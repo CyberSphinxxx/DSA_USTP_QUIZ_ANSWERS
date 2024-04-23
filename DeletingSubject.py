059b140bc015a3409b4ec09e027b53da74e8d154
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:42:02 +0800

    Deleting a Subject from a Dictionary

---
+++ DeletingSubject.py
@@ -0,0 +1,10 @@
+#Given the following statement:
+#Grades = {"Math" : 2.0 , "English" : 1.5, "Science" : 1.9, "ComputerProgramming" : 15}
+
+#Create a simple code to delete the English subject from the given values that we declared.
+
+Grades = {"Math": 2.0, "English": 1.5, "Science": 1.9, "ComputerProgramming": 15}
+
+Grades.pop("English")
+
+print(Grades)
