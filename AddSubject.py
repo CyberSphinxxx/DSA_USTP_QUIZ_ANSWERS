7e690bbcea58dccc5bc87adc151fd8459f831e54
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:45:10 +0800

    Adding an Element to a Set in Python

---
+++ AddSubject.py
@@ -0,0 +1,13 @@
+#Given the statements:
+#set1 = {5,10,15,20}
+#set2 = {"Mon", "Tue", "Wed", "Thurs", "Fri"}
+
+#What is the SYNTAX if we want to add "Saturday" in set2?
+#Write the code below. 
+
+set1 = {5, 10, 15, 20}
+set2 = {"Mon", "Tue", "Wed", "Thurs", "Fri"}
+
+set2.add("Saturday")
+
+print(set2)
