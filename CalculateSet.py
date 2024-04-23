71f96440737021809aae9e164d2d060ac9a5a4bd
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:47:53 +0800

    Calculating the Sum of Values in a Set

---
+++ CalculateSet.py
@@ -0,0 +1,6 @@
+# Create a set variable named: set_items3 which contains values : 1,2,3,4,5,6 and PRINT the sum of the values. 
+# Write the code below. 
+
+set_items3 = {1, 2, 3, 4, 5, 6}
+
+print("The sum of the values inside \"set_items3\" is:", sum(set_items3))
