c4e6626b5362a9d83d4c1cc4aea201c169eb10d5
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 20:06:29 +0800

    Printing Maximum Value from a Set

---
+++ MaximumValuePrinter.py
@@ -0,0 +1,6 @@
+#Create a set variable named: set_items1 which contains values : 2, 4, 6, 10, 20, 30 and PRINT the maximum value. 
+#Write the code below. 
+
+set_items1 = {2, 4, 6, 10, 20, 30}
+
+print("The maximum value is:", max(set_items1))
