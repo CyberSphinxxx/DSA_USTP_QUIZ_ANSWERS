bbe8523f82717b35fb8132167e4b0b3151d3d2ad
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:39:38 +0800

    Printing Minimum Value from a Set

---
+++ MinimumValuePrinter.py
@@ -0,0 +1,6 @@
+#Create a set variable named: set_items2 which contains values : 1,5,7,9,11,21,31 and PRINT the minimum value. 
+#Write the code below. 
+
+set_items2 = {1, 5, 7, 9, 11, 21, 31}
+
+print("The minimum value is:", min(set_items2))
