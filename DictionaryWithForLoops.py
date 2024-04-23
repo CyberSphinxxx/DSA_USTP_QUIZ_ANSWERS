4d53a292ffe2d787d241614b53d9e299fd2dcdaf
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 20:17:39 +0800

    Iterating Through Dictionary Keys Using a For Loop

---
+++ DictionaryWithForLoops.py
@@ -0,0 +1,9 @@
+#Using a For loop, iterate through each key of a dictionary given below:
+#marks = {"Math" : 90, "Science" : 89, "English" : 90}
+#Write the code in the space provided.
+
+
+marks = {"Math": 90, "Science": 89, "English": 90}
+
+for key in marks:
+    print(key)
