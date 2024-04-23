3d54d42ccb7f9d7318700c134fed34444b7c1d3e
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:24:09 +0800

    TupSlice: Tuple Value Extraction Code

---
+++ TupSlice.py
@@ -0,0 +1,8 @@
+# In a given declarations: my_favorite_hobby = ("singing", "eating", "travelling", "camping", "studying", "reporting")
+# What if I want to slice the values of this tuples, and I want to print only the  "camping", "studying", "reporting" .
+# What will be the proper code to get that result? Write the code in the space below.
+
+my_favorite_hobby = ("singing", "eating", "travelling", "camping", "studying", "reporting")
+
+sliced_values = my_favorite_hobby[3:]
+print(sliced_values)
