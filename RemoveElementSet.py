7792bbd351e7a27f828e7b3abb81c82adc269421
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:54:37 +0800

    Removing an Element from a Set in Python

---
+++ RemoveElementSet.py
@@ -0,0 +1,13 @@
+# Given the statements:
+# set1 = {5,10,15,20}
+# set2 = {"Mon", "Tue", "Wed", "Thurs", "Fri"}
+
+# What is the SYNTAX if we want to remove 20 in set1?
+# Write the code below. 
+
+set1 = {5, 10, 15, 20}
+set2 = {"Mon", "Tue", "Wed", "Thurs", "Fri"}
+
+set1.remove(20)
+
+print(set1)
