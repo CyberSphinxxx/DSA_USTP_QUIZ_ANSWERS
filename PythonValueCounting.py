a1eb6a8c7071f9a3409bff4264ef216ba7fe44da
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:26:02 +0800

    Counting Values in Python Declarations

---
+++ PythonValueCounting.py
@@ -0,0 +1,12 @@
+# From the given statements: 
+# my_friends = ["Ana", "Elsa", "Jazmine", "Cindy", "Amia"]
+# my_friends_age = (14, 16, 15, 18, 19)
+
+# What is the syntax if I want to COUNT the number of values from the given declarations?
+# Write the code below. 
+
+my_friends = ["Ana", "Elsa", "Jazmine", "Cindy", "Amia"]
+my_friends_age = (14, 16, 15, 18, 19)
+
+print("Number of friends:", len(my_friends))
+print("Number of ages:", len(my_friends_age))
