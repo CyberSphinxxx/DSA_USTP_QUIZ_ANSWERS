9a3a1a0bd3d5d5f15633564b848ca2cedfbf42da
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 20:10:23 +0800

    Creating a Dictionary Variable: Grades

---
+++ GradesDictionary.py
@@ -0,0 +1,6 @@
+# Create a Dictionary Variable name: Grades.
+# Be sure to put also the keys and values of this Dictionary.
+
+My_Grades = {"MMW": 1.25, "DSA": 1.25, "ComProg": 1.50, "ArtApp": 2.75}
+
+print(My_Grades)
