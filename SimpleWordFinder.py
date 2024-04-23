05bf7a8dc3085171c1e9fa5b45b93199489476ca
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 19:27:49 +0800

    Checking for a specific word in a Tuple

---
+++ SimpleWordFinder.py
@@ -0,0 +1,10 @@
+# In a given statement: my_tuple_animals = ("dog", "cat", "mouse", "horse") 
+# Write the correct code for checking if the CAT exist in the tuple. 
+
+my_tuple_animals = ("dog", "cat", "mouse", "horse")
+
+if "cat" in my_tuple_animals:
+    print("The CAT is in my tuple.")
+
+else:
+    print("The CAT does not exist in my tuple.")
