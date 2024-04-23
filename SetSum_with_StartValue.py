89f2b6515eb476bbd56c7368759b1ef8ea9d59fc
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-23 20:13:36 +0800

    Calculating the Sum of Set Values with a Start Value

---
+++ SetSum_with_StartValue.py
@@ -0,0 +1,11 @@
+#Given the following set:
+#set = {1,2,3,4,5}
+
+#Create a sum syntax/code in which the value of start is 10.
+#Then print the sum. 
+
+set = {1, 2, 3, 4, 5}
+startVal = 10
+
+result = sum(set, startVal)
+print("The sum is:", result)
